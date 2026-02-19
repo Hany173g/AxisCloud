875608484cb178980cff75a30784c004e273b965
Hany <hanykholey0@gmail.com>
2026-02-19 17:58:24 +0300

    fix dockerfile

---
+++ Backend/jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+export default {
+  testEnvironment: 'node',
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+};
---
+++ Backend/tests/sum.test.ts
@@ -0,0 +1,11 @@
+import { sum } from './sum.js';
+import {describe, expect, test} from '@jest/globals';
+
+
+
+
+describe("sum module", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });  
+})
